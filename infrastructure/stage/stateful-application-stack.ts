a5bc8aa5b71f3b67b280c62ba94efa652d1e7644
Alexis Lucattini <alexis.lucattini@umccr.org>
2026-08-21 00:45:23 +0000

    feat: migrate to platform-cdk-constructs 1.7.2 and adopt GitStack (#69)
    - Bump @orcabus/platform-cdk-constructs from 1.7.0 to 1.7.2
    - Stateful and stateless application stacks now extend GitStack
      instead of cdk.Stack, adding a CfnOutput with the git commit ID
      to each deployed stack for deployment traceability

--- infrastructure/stage/stateful-application-stack.ts
+++ infrastructure/stage/stateful-application-stack.ts
@@ -12,10 +12,11 @@ import {
 import { NagSuppressions } from 'cdk-nag';
 import { buildSsmParameters } from './ssm';
 import { buildSchemas } from './event-schemas';
+import { GitStack } from '@orcabus/platform-cdk-constructs/deployment-stack-pipeline';
 
 export type StatefulApplicationStackProps = cdk.StackProps & StatefulApplicationStackConfig;
 
-export class StatefulApplicationStack extends cdk.Stack {
+export class StatefulApplicationStack extends GitStack {
   constructor(scope: Construct, id: string, props: StatefulApplicationStackProps) {
     super(scope, id, props);
 
